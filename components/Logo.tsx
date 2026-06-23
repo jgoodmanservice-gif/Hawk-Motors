54a1cafa7ca78917a7bdaa14542cc520f102cc63
Josh Goodman <joshuagoodman2001@gmail.com>
2026-06-23 16:44:58 +0100

    fix: add style prop to Logo component to fix build error

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,16 @@
+import React from "react";
+
 export function Logo({
   className = "",
+  style,
 }: {
   variant?: "mark" | "full";
   className?: string;
   withText?: boolean;
+  style?: React.CSSProperties;
 }) {
   return (
     // eslint-disable-next-line @next/next/no-img-element
-    <img src="/logo.png" alt="Hawk Motors" className={className} />
+    <img src="/logo.png" alt="Hawk Motors" className={className} style={style} />
   );
 }
