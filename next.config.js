e614d4f7a5100afca1dbbac32b4d89675143f575
Josh Goodman <joshuagoodman2001@gmail.com>
2026-06-24 10:25:16 +0100

    security: disable X-Powered-By response header

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
   reactStrictMode: true,
   images: {
     // Allow remote sample imagery + your future CDN / cloud storage bucket.
